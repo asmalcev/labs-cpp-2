92d973a0ab45002c13540f6a2b93420195a41d06
alex <qzerrty@protonmail.com>
2020-11-14 15:25:09 +0300

    lab2 is done

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,118 +1,97 @@
-﻿#include <windows.h>
+﻿#include <Windows.h>
+#include <wingdi.h>
+#include <sstream>
 #include <gdiplus.h>
 
-#include <cstdlib>
-#include <iostream>
+int color = 0;
 
-#define WINDOW_CLASS L"AppClass"
-
-BYTE red = 255, green = 0, blue = 255;
-bool flag = false;
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMSG, WPARAM wParam, LPARAM lParam)
 {
+	PAINTSTRUCT ps;
 	HDC hdc;
-	switch (msg)
+	switch (uMSG)
 	{
+	case WM_CREATE:
+	{
+		std::stringstream ss;
+		DWORD version = GetVersion();
+
+		ss << "Made by Malcev Alexander I595 WinApi Version: ";
+		ss << version;
+		CreateWindow(
+				TEXT("STATIC"),
+				TEXT(ss.str().c_str()),
+				WS_VISIBLE | WS_CHILD,
+				90, 32, 230, 32, hwnd, nullptr, nullptr, nullptr);
+	}
+	break;
+
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hwnd, &ps);
+		Gdiplus::Graphics gf(hdc);
+		Gdiplus::SolidBrush brush(Gdiplus::Color(255, color, color, color));
+
+		gf.FillRectangle(&brush, 168, 200, 64, 64);
+		EndPaint(hwnd, &ps);
+	}
+	break;
+
+	case WM_LBUTTONDOWN:
+		color += 30;
+		InvalidateRect(hwnd, nullptr, true);
+		break;
+
 	case WM_DESTROY:
 		PostQuitMessage(EXIT_SUCCESS);
 		return 0;
 
-	case WM_LBUTTONDOWN:
-		if (flag)
-		{
-			red = 0;
-			green = 255;
-			blue = 0;
-		}
-		else
-		{
-			red = 255;
-			green = 0;
-			blue = 255;
-		}
-		flag = !flag;
-		break;
-
-	case WM_PAINT:
-		draw(red, green, blue);
-		break;
-
 	default:
-		return DefWindowProc(hwnd, msg, wparam, lparam);
+		break;
 	}
+	return DefWindowProc(hwnd, uMSG, wParam, lParam);
 }
 
-void draw(BYTE r, BYTE g, BYTE b)
+int CALLBACK WinMain(HINSTANCE currentInstance, HINSTANCE, LPSTR cmdLine, int nCmdShow)
 {
-	hdc = BeginPaint(hWnd, &ps);
-	WCHAR szTest[10];
-	DWORD Version = GetVersion();
-	swprintf_s(szTest, 10, L"%d", Version);
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
 
-
-	wsprintfW(szBuff, L"WinApi version: %d.%d\0", dwMajorVersion, dwMinorVersion);
-
-	HDC hdc;
-	PAINTSTRUCT ps;
-	LOGFONT lf;
-	HFONT hFont;
-	DWORD Version = GetVersion();
-	WCHAR szTest[10];
-	swprintf_s(szTest, 10, L"%d", Version);
-
-	hdc = BeginPaint(hWnd, &ps);
-
-	lf.lfHeight = 20;
-	lf.lfItalic = 1;
-	lf.lfStrikeOut = 0;
-	lf.lfUnderline = 0;
-	lf.lfWidth = 10;
-	lf.lfWeight = 40;
-	lf.lfCharSet = DEFAULT_CHARSET;
-	lf.lfPitchAndFamily = DEFAULT_PITCH;
-	lf.lfEscapement = 0;
-	hFont = CreateFontIndirect(&lf);
-	SelectObject(hdc, hFont);
-	SetTextColor(hdc, RGB(0, 0, 0));
-
-	TextOut(hdc, 80, 60, L"Malcev A.S. I595", 11);
-	TextOut(hdc, 80, 80, L"Version WinAPI:", 15);
-	TextOut(hdc, 250, 80, szTest, 10);
-	recten(hdc);
-
-	ValidateRect(hWnd, NULL);
-	EndPaint(hWnd, &ps);
-}
-
-int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmdShow)
-{
 	MSG msg{};
 	HWND hwnd{};
-	WNDCLASS wc{sizeof(WNDCLASS)};
+	WNDCLASSEX wc{sizeof(WNDCLASSEX)};
 
-	const char *CLASS_NAME = "";
-	wc.hInstance = hInstance;
-	wc.lpszClassName = WINDOW_CLASS;
+	wc.hInstance = currentInstance;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.lpszClassName = "LAB2";
 	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wc.lpfnWndProc = WindowProc;
-	if (!RegisterClass(&wc))
-		return EXIT_FAILURE;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszMenuName = nullptr;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
 
-	hwnd = CreateWindow(wc.lpszClassName, "WinAPI 32",
+	if (!RegisterClassEx(&wc))
+	{
+		return EXIT_FAILURE;
+	}
+
+	hwnd = CreateWindow(wc.lpszClassName, "LAB2",
 											WS_OVERLAPPEDWINDOW,
-											100, 100,
-											800, 600,
+											CW_USEDEFAULT, CW_USEDEFAULT,
+											400, 400,
 											nullptr, nullptr, wc.hInstance, nullptr);
-	if (hwnd == INVALID_HANDLE_VALUE)
-		return EXIT_FAILURE;
-
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
 
 	while (GetMessage(&msg, nullptr, 0, 0))
 	{
 		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		DispatchMessageA(&msg);
 	}
+	Gdiplus::GdiplusShutdown(gdiplusToken);
+	return (int)msg.wParam;
 }
